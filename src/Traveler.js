dc945b6919c1a3d9890da21aa21da8ad7a2c3291
Robert Aiken <robertsiraaiken@gmail.com>
2023-01-11 10:50:34 -0600

    Add traveler test

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,11 +1,7 @@
 class Traveler {
   constructor(travelerData, userID){
-    this.traveler = travelerData.travelers.filter(traveler => traveler.id === userID)
-  }
-  returnTravelerName(){
-    return this.traveler.name
-  }
-  returnTravelerType(){
-    return this.traveler.travelerType
+    this.traveler = travelerData.travelers.find(traveler => traveler.id === userID)
   }
 }
+
+export default Traveler
---
+++ test/Traveler-test.js
@@ -0,0 +1,47 @@
+import chai, { expect } from 'chai';
+import Traveler from "../src/Traveler"
+
+describe("Traveler",() => {
+  let userTraveler;
+
+  beforeEach(()=>{
+    let travelerData = {
+      travelers: [{
+        id: 1,
+        name: "Ham Leadbeater",
+        travelerType: "relaxer"
+        },
+        {
+        id: 2,
+        name: "Rachael Vaughten",
+        travelerType: "thrill-seeker"
+        },
+        {
+        id: 3,
+        name: "Sibby Dawidowitsch",
+        travelerType: "shopper"
+        }]
+    } 
+
+  userTraveler = new Traveler(travelerData,2)
+
+  })
+
+  it("should be a function", function(){
+    expect(Traveler).to.be.a("function")
+  })
+
+  it("should be an instance of Traveler",function(){
+    expect(userTraveler).to.be.an.instanceOf(Traveler)
+  })
+
+  it("should have a traveler",function(){
+    expect(userTraveler.traveler).to.deep.equal({
+      id: 2,
+      name: "Rachael Vaughten",
+      travelerType: "thrill-seeker"
+      })
+  })
+
+
+})
